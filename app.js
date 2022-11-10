1c95b2bc4a931343fdf6c54e1f145c73e29aa307
DeAndre' White <111456327+DWhite3112@users.noreply.github.com>
2022-11-09 21:25:30 -0600

    add model changes to app.js

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var King = require("./models/king");
+
+// Atlas Mongo DB connection
+require('dotenv').config();
+const connectionString =
+process.env.MONGO_CON
+mongoose = require('mongoose');
+mongoose.connect(connectionString,
+{useNewUrlParser: true,
+useUnifiedTopology: true});
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -42,4 +53,36 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+const { Server } = require('http');
+const { start } = require('repl');
+
+//server start
+async function recreatDB(){
+  await King.delete.Many();
+
+  let insastance1 = new
+  King({name:"Dree", kingdom:'the world',
+years_ruled:300});
+instance1.save( function(err,doc) {
+if(err) return console.error(err);
+console.log("First object saved")
+});
+  let insastance2 = new
+  King({name:"Alexander", kingdom:'Macedonia',
+years_ruled:33});
+instance1.save( function(err,doc) {
+if(err) return console.error(err);
+console.log("First object saved")
+});
+  let insastance3 = new
+  King({name:"Alfred", kingdom:'Wessex',
+years_ruled:51});
+instance1.save( function(err,doc) {
+if(err) return console.error(err);
+console.log("First object saved")
+});
+}
+let reseed = true;
+if (reseed) { recreateDB();
+}
 module.exports = app;
