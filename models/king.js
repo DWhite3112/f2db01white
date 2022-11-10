c81e5fc93872de4119f1653d52f1b66127670308
DeAndre' White <111456327+DWhite3112@users.noreply.github.com>
2022-11-09 21:54:22 -0600

    add instances to king.js

---
+++ models/king.js
@@ -0,0 +1,29 @@
+//server start
+async function recreatDB(){
+    await King.delete.Many();
+  
+    let insastance1 = new
+    King({name:"Dree", kingdom:'the world',
+  years_ruled:300});
+  instance1.save( function(err,doc) {
+  if(err) return console.error(err);
+  console.log("First object saved")
+  });
+    let insastance2 = new
+    King({name:"Alexander", kingdom:'Macedonia',
+  years_ruled:33});
+  instance1.save( function(err,doc) {
+  if(err) return console.error(err);
+  console.log("First object saved")
+  });
+    let insastance3 = new
+    King({name:"Alfred", kingdom:'Wessex',
+  years_ruled:51});
+  instance1.save( function(err,doc) {
+  if(err) return console.error(err);
+  console.log("First object saved")
+  });
+  }
+  let reseed = true;
+  if (reseed) { recreateDB();
+  }
