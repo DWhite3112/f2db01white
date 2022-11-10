c0daf622d294ac0682d7d4647b665c67645ef573
DeAndre' White <111456327+DWhite3112@users.noreply.github.com>
2022-11-09 23:04:42 -0600

    trying to fix instances

--- app.js
+++ app.js
@@ -54,6 +54,35 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+//server start
+async function recreatDB(){
+  await King.delete.Many();
+
+  let insastance1 = new
+  King({name:"Dree", kingdom:'the world',
+years_ruled:300});
+instance1.save( function(err,doc) {
+if(err) return console.error(err);
+console.log("First object saved")
+});
+  let insastance2 = new
+  King({name:"Alexander", kingdom:'Macedonia',
+years_ruled:33});
+instance1.save( function(err,doc) {
+if(err) return console.error(err);
+console.log("First object saved")
+});
+  let insastance3 = new
+  King({name:"Alfred", kingdom:'Wessex',
+years_ruled:51});
+instance1.save( function(err,doc) {
+if(err) return console.error(err);
+console.log("First object saved")
+});
+}
+let reseed = true;
+if (reseed) {recreatDB();
+}
 
 const { Server } = require('http');
 const { start } = require('repl');
