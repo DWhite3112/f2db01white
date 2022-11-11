b53c6bd31c640cc9016bfdfee7fc2f80c28c9a92
DeAndre' White <111456327+DWhite3112@users.noreply.github.com>
2022-11-11 11:10:10 -0600

    modified resource to try a fix

--- routes/resource.js
+++ routes/resource.js
@@ -6,15 +6,15 @@ var king_controller = require('../controllers/king');
 /// API ROUTE ///
 // GET resources base.
 router.get('/', api_controller.api);
-/// COSTUME ROUTES ///
-// POST request for creating a Costume.
+/// King ROUTES ///
+// POST request for creating a King.
 router.post('/king', king_controller.king_create_post);
-// DELETE request to delete Costume.
+// DELETE request to delete King.
 router.delete('/king/:id', king_controller.king_delete);
-// PUT request to update Costume.
+// PUT request to update King.
 router.put('/king/:id', king_controller.king_update_put);
-// GET request for one Costume.
+// GET request for one King.
 router.get('/king/:id', king_controller.king_detail);
-// GET request for list of all Costume items.
+// GET request for list of all King items.
 router.get('/king', king_controller.king_list);
 module.exports = router;
