80ff2da3cb57e4753edda0dc26ca0076534fd7a0
DeAndre' White <111456327+DWhite3112@users.noreply.github.com>
2022-11-11 13:57:12 -0600

    lab12

--- controllers/king.js
+++ controllers/king.js
@@ -1,12 +1,20 @@
 var King = require('../models/king');
+
 // List of all King
 exports.king_list = function(req, res) {
 res.send('NOT IMPLEMENTED: King list');
 };
 // for a specific King.
-exports.king_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: King detail: ' + req.params.id);
-};
+exports.king_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await King.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
 // Handle King create on POST.
 exports.king_create_post = function(req, res) {
 res.send('NOT IMPLEMENTED: King create POST');
@@ -16,9 +24,25 @@ exports.king_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: King delete DELETE ' + req.params.id);
 };
 // Handle King update form on PUT.
-exports.king_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: King update PUT' + req.params.id);
-};
+exports.king_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await King.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.king_type)
+    toUpdate.king_type = req.body.king_type;
+    if(req.body.cost) toUpdate.cost = req.body.cost;
+    if(req.body.size) toUpdate.size = req.body.size;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
+    }
+    };
 // List of all King
 exports.king_list = async function(req, res) {
     try{
@@ -42,3 +66,9 @@ res.status(500);
 res.send(`{"error": ${err}}`);
 }
 };
+var express = require('express');
+const king_controller= require('./controllers/king')
+var router = express.Router();
+/* GET kings */
+router.get('/', king_controller.king_view_all_Page );
+module.exports = router;
